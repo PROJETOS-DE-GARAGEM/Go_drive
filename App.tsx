a98deee23d2037502192cc0728b1bf81182e419f
railsoncosmo <railson_sousa10@icloud.com>
2025-03-30 19:52:46 -0300

    fix:adjusting component statusbar typing error

--- App.tsx
+++ App.tsx
@@ -1,22 +1,19 @@
 import {
-  Platform,
-  SafeAreaView,
-  StatusBar,
   StyleSheet,
   View,
 } from "react-native";
-import BgGradient from "./src/components/BgGradient/BgGradientStyle";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import Welcome from "./src/pages/Welcome/Welcome";
 import Login from "./src/pages/Register/Register";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 import { CustomStatusBar } from "./src/components/CustomStatusbar";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <CustomStatusBar backgroundColor="#1f51d8" barStyle="ligth-content" />
+      <CustomStatusBar backgroundColor="#1f51d8" barStyle="light-content" />
       <View style={styles.container}>
-        <Login />
+        {/* <Welcome/> */}
+        <Login/>
       </View>
     </SafeAreaProvider>
   );
--- src/components/CustomStatusbar/index.tsx
+++ src/components/CustomStatusbar/index.tsx
@@ -1,26 +1,19 @@
-import { View, StatusBar } from "react-native";
-import {
-  SafeAreaView,
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from "react-native-safe-area-context";
+import { View, StatusBar, StatusBarStyle } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 type StatusbarProps = {
-    backgroundColor: string
+  backgroundColor: string;
+  barStyle: StatusBarStyle;
+};
 
-}
-
- const CustomStatusBar = ({ backgroundColor}: StatusbarProps) => {
+const CustomStatusBar = ({ backgroundColor, barStyle }: StatusbarProps) => {
   const insets = useSafeAreaInsets();
 
   return (
-    <View style={{ height: insets.top, backgroundColor}}>
-      <StatusBar 
-        backgroundColor={backgroundColor}
-        barStyle="light-content"
-    />
+    <View style={{ height: insets.top, backgroundColor }}>
+      <StatusBar backgroundColor={backgroundColor} barStyle={barStyle} />
     </View>
   );
 };
 
-export { CustomStatusBar}
+export { CustomStatusBar };
