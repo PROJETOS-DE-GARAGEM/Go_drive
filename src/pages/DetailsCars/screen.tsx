84b91f39c06a3faf6a2e5cd31931768d88247af4
Isaac Gomes <isaacgomes932@gmail.com>
2025-04-09 17:09:01 -0300

    Base Screen DetailsCars

---
+++ src/pages/DetailsCars/screen.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, View } from 'react-native';
+import ButtonIcon from '../../components/ButtonIcon/ButtonIcon'
+import  styles  from './detailsCarsStyles'
+
+interface DataDetailsCar {
+    title: string;
+    descrition: string;
+    quality: string;
+    kmRound: number;
+}
+
+export default function DetailsCar() {
+  return (
+    <View style={null}>
+      {/* componentHeader ou Nav*/}
+      <View>
+        {/* Image car */}
+        {/* Marca */}
+        {/* Pré descriptions */}
+        {/* <ButtonIcon iconName='star' iconSize={10} iconColor='yellow'/> */}
+      </View>
+
+      <View>
+        {/* CharactersTitle */}
+        <View>
+            {/* <ButtonIcon iconName='event-seat' iconSize={10}iconColor='gray'/> */}
+            {/* nameCharacters */}
+            {/* text */}
+        </View>
+      </View>
+
+      {/* button alugarCarro */}
+    </View>
+  );
+}
