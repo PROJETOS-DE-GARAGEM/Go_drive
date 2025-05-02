a0c8ee4814a28f145e89009929711df3024c6c70
ernandotome <ernandotome@hotmil.com>
2025-05-02 01:30:04 -0300

    First profile screen

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,75 @@
-import { View, Text } from "react-native";
+import React, { useState } from 'react';
+import { View, Text, TextInput, Image, TouchableOpacity, Button, SafeAreaView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import styles from './style';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function Profile() {
+
+const ProfileScreen = () => {
+  const navigation = useNavigation();
+  const [firstName, setFirstName] = useState('Ernando');
+  const [lastName, setLastName] = useState('Tomé');
+  const [email, setEmail] = useState('ernando@gmail.com');
+
+  const handleSave = () => {
+
+    console.log({ firstName, lastName, email });
+  };
+
   return (
-    <View>
-      <Text> Page Profile</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Ionicons name="arrow-back" size={30} color="black" />
+        </TouchableOpacity>
+        
+        <Text style={styles.title}>Profile</Text>
+        
+        <TouchableOpacity>
+          <Ionicons name="create-outline" size={35} color="black"/>
+          <Text style={styles.editText}>Editar</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.divider}/>
+
+      <View style={styles.profileSection}>
+        <View style={styles.avatarContainer}>
+          <Image source={require('../../../assets/avatar.png')} style={styles.avatar} />
+          <TouchableOpacity style={styles.editAvatarIcon}>
+            <Ionicons name="create-outline" size={28} color="black" />
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.profileName}>{`${firstName} ${lastName}`}</Text>
+      </View>
+
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          value={firstName}
+          onChangeText={setFirstName}
+          placeholder="First Name"
+        />
+        <TextInput
+          style={styles.input}
+          value={lastName}
+          onChangeText={setLastName}
+          placeholder="Last Name"
+        />
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={setEmail}
+          placeholder="Email"
+        />
+      </View>
+
+      <TouchableOpacity style={styles.saveButton} onPress={handleSave}>
+        <Text style={styles.saveButtonText}>Salvar</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
-}
+};
+
+export default ProfileScreen;
---
+++ src/pages/Profile/style.tsx
@@ -0,0 +1,86 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 30,
+    paddingTop: 70,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: '8%',
+    marginBottom: 10,
+    
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    alignItems: 'center',
+    margin: 'auto',
+  },
+  editText: {
+    fontSize: 18,
+    color: 'black',
+    right: 5,
+  },
+  profileSection: {
+    alignItems: 'center',
+    marginBottom: 100,
+    marginTop: 30,
+  },
+  avatar: {
+    width: 150,
+    height: 150,
+    borderRadius: 50,
+    alignSelf: 'center',
+  },
+  avatarContainer: {
+    width: '100%'
+  },
+  editAvatarIcon: {
+    position: 'absolute',
+    bottom: 24,
+    right: 150,
+    backgroundColor: '#EEE',
+    borderRadius: 50,
+    padding: 4,
+  },
+  profileName: {
+    marginTop: 10,
+    fontSize: 30,
+    fontWeight: '600',
+    width: '60%',
+    textAlign: 'center',
+  },
+  form: {
+    marginBottom: 24,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 20,
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    marginBottom: 25,
+    fontSize: 20,
+  },
+  saveButton: {
+    backgroundColor: '#2454E0',
+    paddingVertical: 20,
+    borderRadius: 30,
+    alignItems: 'center',
+    marginTop: 'auto'
+  },
+  saveButtonText: {
+    color: '#fff',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  divider: {
+    height: 1,
+    backgroundColor: '#ccc', 
+    marginVertical: 12,
+  },  
+});
