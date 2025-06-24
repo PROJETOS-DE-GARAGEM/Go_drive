bb654570f7137844551b127b97633e7456054539
Ruan <rugcosta1234@gmail.com>
2025-06-24 20:33:53 -0300

    feat: update app configuration and enhance Header component with customizable right icon

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,26 +2,51 @@ import { View, Text, TouchableOpacity } from "react-native";
 import styles from "./style";
 
 import { useNavigation } from "@react-navigation/native";
-import { Feather } from "@expo/vector-icons";
+import { Feather, MaterialIcons } from "@expo/vector-icons";
 
 interface HeaderProps {
   title: string;
+  rightIconName?: string; // nome do ícone (ex: "logout")
+  rightIconLib?: "Feather" | "MaterialIcons"; // biblioteca do ícone
+  onRightIconPress?: () => void; // função ao clicar
+  rightIconColor?: string;
+  rightIconSize?: number;
+  onBackPress?: () => void;
 }
 
-const Header = ({ title }: HeaderProps) => {
+const Header = ({
+  title,
+  rightIconName,
+  rightIconLib,
+  onRightIconPress,
+  rightIconColor,
+  rightIconSize,
+  onBackPress,
+}: HeaderProps) => {
   const navigation = useNavigation();
+
+  const RightIcon = rightIconLib === "Feather" ? Feather : MaterialIcons;
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity 
+      <TouchableOpacity
         style={styles.containerIcon}
-        onPress={() => navigation.goBack()}
+        onPress={onBackPress ? onBackPress : () => navigation.goBack()} // <- aqui
       >
         <Feather name="chevron-left" color={"#545e69"} size={30} />
       </TouchableOpacity>
       <View style={styles.containerTitle}>
         <Text style={styles.title}>{title}</Text>
       </View>
+      {rightIconName && (
+        <TouchableOpacity style={styles.rightIcon} onPress={onRightIconPress}>
+          <RightIcon
+            name={rightIconName as any}
+            size={rightIconSize}
+            color={rightIconColor}
+          />
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
